40cb56ad50d0c8eacaf322c8bfec3b846133a202
Andrew Fernandez <fdez26@ymail.com>
2020-03-23 15:19:52 -0400

    testing reading from file

--- project/iptool.cpp
+++ project/iptool.cpp
@@ -21,120 +21,92 @@ int main (int argc, char** argv)
 		exit(1);        
     }
 
-    // extracting first line
-    int num_regions;
-    if (fgets(str, MAXLEN, fp) != NULL) {
-        pch = strtok(str, " ");
-        num_regions = atoi(pch);
-        if (num_regions > 3) {
-            cout << "Only 3 ROI allowed" << endl;
-            exit(1);
-        }
-    }
-
-    // getting the regions
-    vector<roi> regions;
-    for (int i = 0; i < num_regions; i++) {
-        if (fgets(str, MAXLEN, fp) != NULL) {
-            pch = strtok(str, " ");
-            int x = atoi(pch);
-            pch = strtok(NULL, " ");
-            int y = atoi(pch);
-            pch = strtok(NULL, " ");
-            int sx = atoi(pch);
-            pch = strtok(NULL, " ");
-            int sy = atoi(pch);
-            pch = strtok(NULL, " ");
-            int a = atoi(pch);
-            pch = strtok(NULL, " ");
-            int b = atoi(pch);
-            pch = strtok(NULL, " ");
-            int red_a = atoi(pch);
-            pch = strtok(NULL, " ");
-            int red_b = atoi(pch);
-            pch = strtok(NULL, " ");
-            int green_a = atoi(pch);
-            pch = strtok(NULL, " ");
-            int green_b = atoi(pch);
-            pch = strtok(NULL, " ");
-            int blue_a = atoi(pch);
-            pch = strtok(NULL, " ");
-            int blue_b = atoi(pch);
-            pch = strtok(NULL, " ");
-            int rgb_a = atoi(pch);
-            pch = strtok(NULL, " ");
-            int rgb_b = atoi(pch);
-            roi new_region = roi(x, y, sx, sy, a, b, 
-                red_a, red_b, green_a, green_b, blue_a, blue_b, rgb_a, rgb_b);
-            regions.push_back(new_region);
-        }
-    }
-
     while (fgets(str, MAXLEN, fp) != NULL) {
         pch = strtok(str, " ");
         src.read(pch); // creating the source image
         string src_name = pch;
 
-        pch = strtok(NULL, " ");
+        pch = strtok(str, " ");
         strcpy(outfile, pch);
+
+        pch = strtok(str , " ");
+        string func_name = pch;
 
         pch = strtok(NULL, " ");
         pch[strlen(pch) - 1] = '\0';
-        if (strncasecmp(pch, "hist_stretch", MAXLEN) == 0) {
-            string file_name = outfile;
-            auto start = chrono::high_resolution_clock::now();
-            utility::histStretch(src, tgt, regions, file_name);
-            auto end = chrono::high_resolution_clock::now();
-            cout << "histStretch time for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
-        }   
-        else if (strncasecmp(pch, "opt_thresh", MAXLEN) == 0) {
-            auto start = chrono::high_resolution_clock::now();
-            utility::optimalThresholding(src, tgt, regions);
-            auto end = chrono::high_resolution_clock::now();
-            cout << "optimalThresholding for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
-        }
-        else if (strncasecmp(pch, "comb_hist_thresh", MAXLEN) == 0) {
-            string file_name = outfile;
-            auto start = chrono::high_resolution_clock::now();
-            utility::combHistOptThresh(src, tgt, regions, file_name);
-            auto end = chrono::high_resolution_clock::now();
-            cout << "combHistOptThresh for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
-        }
-        else if (strncasecmp(pch, "hist_red", MAXLEN) == 0) {
-            string file_name = outfile;
-            auto start = chrono::high_resolution_clock::now();
-            utility::histRed(src, tgt, regions, file_name);
-            auto end = chrono::high_resolution_clock::now();
-            cout << "histRed for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
-        }
-        else if (strncasecmp(pch, "hist_green", MAXLEN) == 0) {
-            string file_name = outfile;
-            auto start = chrono::high_resolution_clock::now();
-            utility::histGreen(src, tgt, regions, file_name);
-            auto end = chrono::high_resolution_clock::now();
-            cout << "histGreen for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
-        }
-        else if (strncasecmp(pch, "hist_blue", MAXLEN) == 0) {
-            string file_name = outfile;
-            auto start = chrono::high_resolution_clock::now();
-            utility::histBlue(src, tgt, regions, file_name);
-            auto end = chrono::high_resolution_clock::now();
-            cout << "histBlue for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
-        }
-        else if (strncasecmp(pch, "hist_rgb", MAXLEN) == 0) {
-            string file_name = outfile;
-            auto start = chrono::high_resolution_clock::now();
-            utility::histRGB(src, tgt, regions, file_name);
-            auto end = chrono::high_resolution_clock::now();
-            cout << "histRGB for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
-        }
-        else {
-            cout << "No function: " << pch << endl;
-            continue;
-        }
+        int num_regions = atoi(pch);
 
-        tgt.save(outfile);
+        cout << "src_name = " << src_name << endl;
+        cout << "tgt_name = " << outfile << endl;
+        cout << "func_name = " << func_name << endl;
+        cout << "num_regions = " << num_regions << endl;
     }
+
+    // while (fgets(str, MAXLEN, fp) != NULL) {
+    //     pch = strtok(str, " ");
+    //     src.read(pch); // creating the source image
+    //     string src_name = pch;
+
+    //     pch = strtok(NULL, " ");
+    //     strcpy(outfile, pch);
+
+    //     pch = strtok(NULL, " ");
+    //     pch[strlen(pch) - 1] = '\0';
+    //     if (strncasecmp(pch, "hist_stretch", MAXLEN) == 0) {
+    //         string file_name = outfile;
+    //         auto start = chrono::high_resolution_clock::now();
+    //         utility::histStretch(src, tgt, regions, file_name);
+    //         auto end = chrono::high_resolution_clock::now();
+    //         cout << "histStretch time for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+    //     }   
+    //     else if (strncasecmp(pch, "opt_thresh", MAXLEN) == 0) {
+    //         auto start = chrono::high_resolution_clock::now();
+    //         utility::optimalThresholding(src, tgt, regions);
+    //         auto end = chrono::high_resolution_clock::now();
+    //         cout << "optimalThresholding for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+    //     }
+    //     else if (strncasecmp(pch, "comb_hist_thresh", MAXLEN) == 0) {
+    //         string file_name = outfile;
+    //         auto start = chrono::high_resolution_clock::now();
+    //         utility::combHistOptThresh(src, tgt, regions, file_name);
+    //         auto end = chrono::high_resolution_clock::now();
+    //         cout << "combHistOptThresh for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+    //     }
+    //     else if (strncasecmp(pch, "hist_red", MAXLEN) == 0) {
+    //         string file_name = outfile;
+    //         auto start = chrono::high_resolution_clock::now();
+    //         utility::histRed(src, tgt, regions, file_name);
+    //         auto end = chrono::high_resolution_clock::now();
+    //         cout << "histRed for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+    //     }
+    //     else if (strncasecmp(pch, "hist_green", MAXLEN) == 0) {
+    //         string file_name = outfile;
+    //         auto start = chrono::high_resolution_clock::now();
+    //         utility::histGreen(src, tgt, regions, file_name);
+    //         auto end = chrono::high_resolution_clock::now();
+    //         cout << "histGreen for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+    //     }
+    //     else if (strncasecmp(pch, "hist_blue", MAXLEN) == 0) {
+    //         string file_name = outfile;
+    //         auto start = chrono::high_resolution_clock::now();
+    //         utility::histBlue(src, tgt, regions, file_name);
+    //         auto end = chrono::high_resolution_clock::now();
+    //         cout << "histBlue for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+    //     }
+    //     else if (strncasecmp(pch, "hist_rgb", MAXLEN) == 0) {
+    //         string file_name = outfile;
+    //         auto start = chrono::high_resolution_clock::now();
+    //         utility::histRGB(src, tgt, regions, file_name);
+    //         auto end = chrono::high_resolution_clock::now();
+    //         cout << "histRGB for " << src_name << " = " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+    //     }
+    //     else {
+    //         cout << "No function: " << pch << endl;
+    //         continue;
+    //     }
+
+    //     tgt.save(outfile);
+    // }
 
     fclose(fp);
     return 0;
